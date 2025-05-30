c04c14576e9a005bc9936371806bc4d8a933b1bf
shadow-hokague <duboisclement853@gmail.com>
2025-05-30 14:29:18 +0200

    Update and rename config-sample.php to config.php

---
+++ user/config.php
@@ -0,0 +1,16 @@
+<?php
+define( 'YOURLS_DB_USER', getenv('DB_USER') );
+define( 'YOURLS_DB_PASS', getenv('DB_PASS') );
+define( 'YOURLS_DB_NAME', getenv('DB_NAME') );
+define( 'YOURLS_DB_HOST', getenv('DB_HOST') );
+
+define( 'YOURLS_SITE', 'https://your-subdomain.onrender.com' );
+define( 'YOURLS_HOURS_OFFSET', 0 );
+define( 'YOURLS_LANGUAGE', 'en' );
+
+define( 'YOURLS_UNIQUE_URLS', true );
+define( 'YOURLS_PRIVATE', true );
+
+$yourls_user_passwords = array(
+  'admin' => '@Motdepasse0000'
+);
